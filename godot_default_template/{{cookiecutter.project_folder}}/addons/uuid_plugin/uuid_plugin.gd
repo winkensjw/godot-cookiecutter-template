c49303b775d9521b2794b6605987ed873b407cb9
winkensjw <winkensjw@gmail.com>
2025-03-03 11:02:36 +0100

    Clean up uuid plugin

--- godot_default_template/{{cookiecutter.project_folder}}/addons/uuid_plugin/uuid_plugin.gd
+++ godot_default_template/{{cookiecutter.project_folder}}/addons/uuid_plugin/uuid_plugin.gd
@@ -1,28 +1,30 @@
 @tool
 extends EditorPlugin
 
+var _short_cut : InputEventKey = _create_shortcut()
+
 func _input(event: InputEvent) -> void:
+	if event.is_match(_short_cut) and event.is_released():
+		_insert_uuid()
+
+func _create_shortcut():
 	var input_event : InputEventKey = InputEventKey.new()
 	input_event.keycode = Key.KEY_U
 	input_event.alt_pressed = true
 	input_event.ctrl_pressed = true
-	input_event.pressed = false # match on release
-	var shortcut : Shortcut = Shortcut.new()
-	shortcut.events.append(input_event)
-	if event.is_match(input_event) and event.is_released():
-		_on_insert_uuid_command()
-
+	return input_event
+	
 func _enter_tree() -> void:
-	EditorInterface.get_command_palette().add_command("Insert UUID", "insert/uuid", _on_insert_uuid_command, "Ctrl+Alt+U")
+	EditorInterface.get_command_palette().add_command("Insert UUID", "insert/uuid", _insert_uuid, "Ctrl+Alt+U")
 
 func _exit_tree() -> void:
 	EditorInterface.get_command_palette().remove_command("insert/uuid")
 
-func generate_uuid() -> String:
-	return UUID.v7()
-
-func _on_insert_uuid_command():
-	_get_editior().insert_text_at_caret(generate_uuid())
+func _insert_uuid() -> void:
+	_get_editior().insert_text_at_caret(_generate_uuid())
 
 func _get_editior() -> TextEdit:
 	return get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
+
+func _generate_uuid() -> String:
+	return UUID.v7()
