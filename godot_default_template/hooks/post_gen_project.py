29b21f728d836d9b23de0c9db86caf36e133182c
winkensjw <winkensjw@gmail.com>
2025-03-14 13:39:18 +0100

    Add audio manager plugin to template

--- godot_default_template/hooks/post_gen_project.py
+++ godot_default_template/hooks/post_gen_project.py
@@ -18,12 +18,18 @@ def add_submodule(submodule_repo, submodule_path):
 # Function to add a symlink from the addons folder to the submodule
 def add_simlink(symlink_target, symlink_name):
 	if not os.path.exists(symlink_name):
+		# manual command: ln -s ../externa/path/to/addon/folder addon_name
 		os.symlink(symlink_target, symlink_name)
 		print("Symlink created successfully")
 	else:
 		print("Symlink already exists")
 
 	print("Submodule added and symlinks created")
+
+def add_addon(addon_repo, addon_external_name, addon_locale_name):
+	add_submodule(addon_repo, "external/" + addon_external_name)
+	add_simlink(os.path.join("..", "external", addon_external_name, "addons", addon_locale_name), os.path.join("addons", addon_locale_name))
+
 
 # Step 1: Initialize the Git repository
 run_command("git init")
@@ -33,11 +39,9 @@ print("Repository initialized")
 run_command("git checkout -b master")
 print("Checked out master branch")
 
-# Step 3: Add the existing submodule
-add_submodule("https://github.com/winkensjw/godot-console.git", "external/godot-console")
-
-# Step 4: Create a symlink
-add_simlink(os.path.join("..", "external", "godot-console", "addons", "console"), os.path.join("addons", "console"))
+# Step 3: Add add ons
+add_addon("https://github.com/winkensjw/godot-console.git", "godot-console", "console")
+add_addon("https://github.com/winkensjw/resonate.git", "resonate", "resonate")
 
 # Step 5: Add all files to the repository
 run_command("git add .")
